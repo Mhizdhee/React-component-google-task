ea73574cb86a3eb9b1ced655ba829a997315866d
mhizdhee <olaniyandorcas91@gmail.com>
2023-04-07 17:49:44 +0100

    updated

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,14 +2,41 @@ import React from 'react'
 import SearchIcon from './SearchIcon';
 import CameraIcon from './CameraIcon';
 import MicrophoneIcon from './MicrophoneIcon';
+import { useState } from 'react'
 
-export default function SearchBar() {
+
+const SearchBar=()=> {
+  const [searchKeyword, setSearchKeyword] = useState('');
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      setSearchKeyword(event.target.value);
+      event.target.value = '';
+    }
+  };
+
+// export default function SearchBar() {
   return (
     <>
-      <input type="text"  />
+      <input type="text" onKeyDown={handleKeyDown} placeholder="Enter search keyword" />
+      {searchKeyword && <p> {searchKeyword}</p>}
       <MicrophoneIcon />
       <CameraIcon />
       <SearchIcon />
     </>
   )
 }
+
+
+
+
+
+//   return (
+//     <div>
+//       <input type="text" onKeyDown={handleKeyDown} placeholder="Enter search keyword" />
+//       {searchKeyword && <p>Search Keyword: {searchKeyword}</p>}
+//     </div>
+//   );
+// }
+
+export default SearchBar;
